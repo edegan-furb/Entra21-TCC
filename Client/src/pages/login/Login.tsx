283bbfcc0c1a895ef9617004b9cc0d1e438f6e48
Jcvanz <j.vanz1108@gmail.com>
2023-11-06 15:25:50 -0300

    Adc arq. Login

---
+++ Client/src/pages/login/Login.tsx
@@ -0,0 +1,18 @@
+import { StyleSheet, View } from 'react-native';
+
+
+export default function Login() {
+    return(
+        <View style={styles.mainContainer}>
+
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    mainContainer: {
+       width: '100%',
+       height: '100%',
+       backgroundColor: '#6C057D' 
+    }
+}) 
