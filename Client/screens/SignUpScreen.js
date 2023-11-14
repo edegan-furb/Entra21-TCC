7d980577bf5351884ae3bd5edd2f02ba3148393b
Julio <j.vanz1108@gmail.com>
2023-11-13 22:07:42 -0300

    test

--- Client/screens/SignUpScreen.js
+++ Client/screens/SignUpScreen.js
@@ -10,7 +10,7 @@ function SignUpScreen() {
           style={styles.background}
         />
         <View style={styles.backButtonContainer}>
-          
+          <Image/>
         </View>
     </View>
   );
