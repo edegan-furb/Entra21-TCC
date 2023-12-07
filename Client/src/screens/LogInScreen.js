b40c37251ccfeeb26bfb294b07ec043b78fc0e96
Julio <j.vanz1108@gmail.com>
2023-12-07 20:41:45 -0300

    padronizando as page de Login e SignUp

--- Client/src/screens/LogInScreen.js
+++ Client/src/screens/LogInScreen.js
@@ -19,6 +19,10 @@ import { GlobalStyles } from "../constants/Colors";
 import { SafeAreaView } from "react-native-safe-area-context";
 import CustomButton from "../components/CustomButton";
 import { LinearGradient } from "expo-linear-gradient";
+import BackButton from "../components/BackButton";
+import PagesTitle from "../components/PagesTitle";
+import Inputs from "../components/Inputs";
+import ButtonGoogleLogin from "../components/ButtonGoogleLogin";
 
 function LogInScreen() {
   const navigation = useNavigation();
@@ -35,94 +39,43 @@ function LogInScreen() {
 
   return (
     <LinearGradient
-      style={styles.linearContainer}
       colors={[
-        GlobalStyles.colors.primary800,
-        GlobalStyles.colors.primary700,
-        GlobalStyles.colors.primary500,
+        GlobalStyles.colors.primary950,
+        GlobalStyles.colors.primary900,
+        GlobalStyles.colors.primary600,
         GlobalStyles.colors.primary300,
-        GlobalStyles.colors.neutral100,
+        GlobalStyles.colors.neutral300,
+        GlobalStyles.colors.neutral300,
       ]}
-      locations={[0, 0.075, 0.15, 0.25, 0.35]}
+      locations={[0.01, 0.05, 0.15, 0.3, 0.4, 0.5]}
+      style={styles.linearContainer}
     >
       <SafeAreaView style={styles.rootContainer}>
-        <View style={styles.backButtonContainer}>
-          <Pressable
-            onPress={() => onPressHandler("Start")}
-            style={({ pressed }) => pressed && styles.pressed}
-          >
-            <Ionicons
-              name={"arrow-back"}
-              size={hp("5%")}
-              color={GlobalStyles.colors.neutral100}
-            />
-          </Pressable>
-        </View>
-        <View style={styles.titleContainer}>
-          <Text style={styles.title}>Welcome!</Text>
-          <Text style={styles.subtitle}>Sign in to continue</Text>
-        </View>
-        <View style={styles.inputOuterContainer}>
-          <View style={styles.inputInnerContainer}>
-            <TextInput style={styles.input} placeholder="email" />
-          </View>
-          <View style={styles.inputInnerContainer}>
-            <TextInput
-              style={styles.input}
-              placeholder="password"
-              secureTextEntry={true}
-            />
-          </View>
-        </View>
-        <View style={styles.loginButton}>
+        <BackButton onPress={() => onPressHandler("Start")}/>
+        <PagesTitle title={'Hello !'} subTitle={'Sign in to continue'}/>
+        <Inputs 
+          placeHolder={'Email'} 
+          placeHolderTextColor={'#555'} 
+          secureTextEntry={false} 
+          height={0}
+          maxLength={60}
+        />
+        <Inputs
+          placeHolder={'Password'} 
+          placeHolderTextColor={'#555'} 
+          secureTextEntry={true} 
+          maxLength={10}
+        />
+
+        <View style={styles.ContentButtons}>
           <CustomButton
-            title={"Login"}
             onPress={() => onPressHandler("Tabs")}
+            title={"LOGIN"}
+            styleButton={styles.ButtonCreateAccount}
+            styleText={styles.textCustomButton}
           />
-          <Pressable style={({ pressed }) => pressed && styles.pressed}>
-            <Text style={styles.buttonText}>Forgot Passwod?</Text>
-          </Pressable>
         </View>
-        <View style={styles.separator}>
-          <View style={styles.divider}></View>
-          <Text style={styles.text}>or</Text>
-          <View style={styles.divider}></View>
-        </View>
-        <View style={styles.otherLogins}>
-          <Text style={styles.socialText}>Social Media Logins</Text>
-          <View style={styles.socialButtons}>
-            <Pressable
-              onPress={() => onPressHandler("Start")}
-              style={({ pressed }) => pressed && styles.pressed}
-            >
-              <Ionicons
-                name={"logo-google"}
-                size={hp("7.5%")}
-                color={"#4285F4"}
-              />
-            </Pressable>
-            <Pressable
-              onPress={() => onPressHandler("Start")}
-              style={({ pressed }) => pressed && styles.pressed}
-            >
-              <Ionicons
-                name={"logo-github"}
-                size={hp("7.5%")}
-                color={"#c9510c"}
-              />
-            </Pressable>
-            <Pressable
-              onPress={() => onPressHandler("Start")}
-              style={({ pressed }) => pressed && styles.pressed}
-            >
-              <Ionicons
-                name={"logo-apple"}
-                size={hp("7.5%")}
-                color={GlobalStyles.colors.neutral900}
-              />
-            </Pressable>
-          </View>
-        </View>
+        <ButtonGoogleLogin/>
         <View style={styles.signUpButton}>
           <Text style={styles.signText}>Don't have an account</Text>
           <Pressable
@@ -141,62 +94,22 @@ export default LogInScreen;
 
 const styles = StyleSheet.create({
   linearContainer: {
-    flex: 1,
     width: wp("100%"),
     height: hp("100%"),
   },
   rootContainer: {
-    flex: 1,
     paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
   },
-  backButtonContainer: {
-    height: hp("7.5%"),
-    marginTop: hp("2%"),
-    marginLeft: wp("5%"),
+  ContentButtons: {
+    paddingVertical: wp('2'),
+    alignItems: "center",
   },
-  pressed: {
-    opacity: 0.7,
+  ButtonCreateAccount: {
+    width: wp('60'),
+    height: hp('6'),
   },
-  titleContainer: {
-    height: hp("12.5%"),
-    marginLeft: wp("10%"),
-  },
-  title: {
-    textAlign: "left",
-    fontSize: hp("5%"),
-    fontFamily: "open-sans-bold",
-    color: GlobalStyles.colors.neutral100,
-    textShadowColor: GlobalStyles.colors.neutral500,
-    textShadowOffset: { width: 1, height: 1 },
-    textShadowRadius: 2,
-  },
-  subtitle: {
-    textAlign: "left",
-    fontSize: hp("2.5%"),
-    fontFamily: "open-sans",
-    color: GlobalStyles.colors.neutral100,
-    textShadowColor: GlobalStyles.colors.neutral500,
-    textShadowOffset: { width: 1, height: 1 },
-    textShadowRadius: 1,
-  },
-  inputOuterContainer: {
-    height: hp("27.5%"),
-    paddingHorizontal: wp("10%"),
-    paddingVertical: hp("5%"),
-  },
-  inputInnerContainer: {
-    borderBottomWidth: wp("0.5%"),
-    borderColor: GlobalStyles.colors.primary900,
-    marginBottom: hp("7.5%"),
-    height: hp("5%"),
-  },
-  input: {
-    width: wp("70%"),
-    fontSize: hp("1.7%"),
-  },
-  loginButton: {
-    height: hp("10%"),
-    paddingHorizontal: wp("15%"),
+  textCustomButton: {
+    fontSize: hp ('1.6%'),
   },
   buttonText: {
     paddingTop: wp("1%"),
@@ -205,43 +118,11 @@ const styles = StyleSheet.create({
     textAlign: "center",
     color: GlobalStyles.colors.primary950,
   },
-  separator: {
-    height: hp(" 10%"),
-    flexDirection: "row",
-    justifyContent: "center",
-    alignItems: "center",
-    gap: wp("5%"),
-  },
-  divider: {
-    height: hp("0.3%"),
-    width: wp("30%"),
-    backgroundColor: GlobalStyles.colors.primary950,
-  },
-  text: {
-    fontSize: hp("2%"),
-    color: GlobalStyles.colors.primary950,
-    fontFamily: "open-sans-bold",
-  },
-  otherLogins: {
-    height: hp("17.5%"),
-  },
-  socialText: {
-    fontSize: hp("2.5%"),
-    color: GlobalStyles.colors.primary950,
-    fontFamily: "open-sans",
-    textAlign: "center",
-  },
-  socialButtons: {
-    flexDirection: "row",
-    justifyContent: "center",
-    alignItems: "center",
-    gap: wp("5%"),
-    marginTop: hp("1%"),
-  },
   signUpButton: {
-    height: hp(" 10%"),
+    height: hp("5%"),
     flexDirection: "row",
     justifyContent: "center",
+    marginTop: 25,
   },
   signText: {
     fontSize: hp("2%"),
@@ -252,5 +133,6 @@ const styles = StyleSheet.create({
     fontSize: hp("2%"),
     fontFamily: "open-sans-bold",
     color: GlobalStyles.colors.primary800,
+    textDecorationLine: 1
   },
 });
